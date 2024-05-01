b33041e8cd169aebbc46c1a373b4edfde823b37d
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-05-01 17:08:14 +0530

    added footer and did some styling on it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,19 +9,32 @@ const Footer = () => {
         <div className="footer-content-left">
             <img className="footerLogo" src={assets.craftLogo}  alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, deleniti voluptatibus nam corrupti est, optio corporis cupiditate non quas fugit beatae neque ipsam.</p>
+            <div className="footer-social-icons">
             <img src={assets.facebookLogo} alt="Facebook" />
             <img src={assets.tXlogo} alt="Twitter" />
             <img src={assets.linkedInLogo} alt="LinkedIn" />
             <img src={assets.instagramLogo} alt="Instagram" />
+            </div>
         </div>
         <div className="footer-content-center">
-
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Privacy & Policy</li>
+          </ul>
         </div>
         <div className="footer-content-right">
-
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>Contact@ashish.com</li>
+          </ul>
         </div>
-        
       </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 &copy; Cyclecraft.com - All Rights Reserved.</p>
     </div>
   )
 }
